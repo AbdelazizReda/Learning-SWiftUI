5393a8411c7efab3eb58b7789a33e8920f593021
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2024-01-21 17:45:30 +0200

    DragGestureMove

--- Learning SWiftUI/day4/DragGestureMove.swift
+++ Learning SWiftUI/day4/DragGestureMove.swift
@@ -10,25 +10,32 @@ import SwiftUI
 struct DragGestureMove: View {
     @State var offset:CGSize = .zero
     var body: some View {
-        
-        Rectangle()
-            .cornerRadius(10)
-            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged({ val in
-                        withAnimation(.spring()){
-                            offset = val.translation
-                        }
-                    })
-                    .onEnded({ val in
-                        withAnimation(.spring()){
-                            offset = .zero
-                        }
-                    })
-            )
-    }
+
+        ZStack{
+            VStack{
+                Text("\(offset.width)")
+                Spacer()
+            }
+            
+            Rectangle()
+                .cornerRadius(10)
+                .frame(width: 200, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .offset(offset)
+                .scaleEffect(0.5)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ val in
+                            withAnimation(.spring()){
+                                offset = val.translation
+                            }
+                        })
+                        .onEnded({ val in
+                            withAnimation(.spring()){
+                                offset = .zero
+                            }
+                        })
+                )
+        }}
 }
 
 #Preview {
