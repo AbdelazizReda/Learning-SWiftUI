8f518a52454a3b110187395aaf1d06c2d79f7c4f
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2024-01-21 14:46:09 +0200

    Day2 - MagnificationGesture
    - MagnificationGesture
    - onChanged
    - onEnded

---
+++ Learning SWiftUI/Day2/MagnificationGesture.swift
@@ -0,0 +1,52 @@
+//
+//  MagnificationGesture.swift
+//  Learning SWiftUI
+//
+//  Created by Abdelaziz Reda on 21/01/2024.
+//
+
+import SwiftUI
+
+struct MagnificationGesture: View {
+    var body: some View {
+        
+        @State var currentAmount:CGFloat = 0
+        @State var lastAmount: CGFloat = 0
+        
+        VStack{
+            HStack {
+                Circle()
+                    .frame(width: 40, height: 40, alignment: .leading)
+                Text("Instgram User")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding(.horizontal)
+            
+            
+            Rectangle()
+                .frame( height: 300, alignment: .leading)
+                .scaleEffect(1 + currentAmount )
+                .gesture(
+//                    MagnificationGesture()
+//                    MagnificationGesture()
+                    MagnifyGesture()
+                        .onChanged {val in
+//                        currentAmount = val - 1
+                        }
+                        .onEnded{ value  in
+                            withAnimation(.spring()){
+                                currentAmount = 0
+                            }
+                        }
+                )
+     
+            
+        }
+
+    }
+}
+
+#Preview {
+    MagnificationGesture()
+}
