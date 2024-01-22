ac9207dc328b2897c1705178ef35d8545eddb9d9
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2024-01-22 16:31:18 +0200

    GeometryReader
    - getPercentage
    - rotation3DEffect

---
+++ Learning SWiftUI/Day5/GeometryReader.swift
@@ -0,0 +1,46 @@
+//
+//  GeometryReader.swift
+//  Learning SWiftUI
+//
+//  Created by Abdelaziz Reda on 22/01/2024.
+//
+
+import SwiftUI
+
+struct GeometryReader: View {
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20 ) { index in
+//                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 10)
+                            .rotation3DEffect(
+//                                Angle(degrees: getPercentage(geo: geometry) * 40)
+                                Angle(degrees: 10),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                            .frame(width: 300, height: 300, alignment: .center)
+                    }
+//                }
+            }
+        }
+
+
+    }
+    
+    
+    //GetPercentage
+    func getPercentage (geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
+    }
+}
+
+#Preview {
+    GeometryReader()
+}
+
+
+
