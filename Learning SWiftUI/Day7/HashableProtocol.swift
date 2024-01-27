08cd7157adc355295f907017e6a4cb8cb4ced901
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2024-01-27 21:59:17 +0200

    hashable

---
+++ Learning SWiftUI/Day7/HashableProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  HashableProtocol.swift
+//  Learning SWiftUI
+//
+//  Created by Abdelaziz Reda on 24/01/2024.
+//
+
+import SwiftUI
+
+struct HashableProtocol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HashableProtocol()
+}
